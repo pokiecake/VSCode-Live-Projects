cbccd97a4e0da776972d4cd8a2ccdc8aacf8b42a
Alan Vo <alanmaivo@gmail.com>
2023-06-03 15:43:23 -0700

    put main2 in main

--- main.py
+++ main.py
@@ -28,15 +28,20 @@ playerDirection = 0
 
 #Apple
 appleImg = pygame.image.load("Assets/Apple.png")
-appleX = random.randint(50, 750)
-appleY = random.randint(50, 350)
+appleW = appleImg.get_width()
+appleH = appleImg.get_height()
 
-#Apple Bullet
+#Apple Bullets
 apple_state = "ready"
+bullets = []
 appleBulletX = 0
 appleBulletY = 0
 apple_changeX = 0
 apple_changeY = 0
+appleBulletCount = 0
+
+#Apple Stockpiles
+stockpiles = []
 
 #Background
 background = pygame.image.load("Assets/spongebob.png")
@@ -47,12 +52,14 @@ def player(x, y):
 def apple(x, y):
     screen.blit(appleImg, (x, y))
 
+#doesn't actually fire apples, only creates them and their positions
 def fire_apple(x, y):
     global apple_state
     global appleBulletX
     global appleBulletY
     global apple_changeX
     global apple_changeY
+    global bullets
     apple_state = "fire"
     xPos = x
     yPos = y
@@ -85,24 +92,70 @@ def fire_apple(x, y):
     #sets the apple bullet's position based on the direction
     appleBulletX = xPos
     appleBulletY = yPos
+    bullet = AppleBullets(xPos, yPos, apple_changeX, apple_changeY)
+    bullets.append(bullet)
 
+#draws anything apple related
 def draw_apple(x, y):
     screen.blit(appleImg, (x, y))
+
+#class for apple bullets
+class AppleBullets:
+    def __init__(self, x, y, change_x, change_y):
+        self.x = x
+        self.y = y
+        self.change_x = change_x
+        self.change_y = change_y
+    
+    def change(self):
+        self.x += self.change_x
+        self.y += self.change_y
+    
+    def getPos(self):
+        return (self.x, self.y)
+
+#class for apple stockpiles
+class AppleStockpiles:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+    
+    #checks for collision in the horizontal and vertical direction
+    def checkCollision(self, pX, pY, pW, pH):
+        if (pY < self.y + self.h and pY + pH > self.y):
+            if (pX < self.x + self.w and pX + pW > self.x) or (pX + pW > self.x and pX < self.x + self.w):
+                return True
+    
+    def getPos(self):
+        return (self.x, self.y)
 
 #background sound
 mixer.music.load('Assets/Clouds.wav')
 mixer.music.set_volume(0.2)
 mixer.music.play(-1)
 
+#Global Variable Setup
+#Key Press Status (held down or not)
+lefthold = False
+righthold = False
+uphold = False
+downhold = False
+
 #Game Loop. When the x button is clicked, running is set to false and the window closes.
 running = True
 while running:
-
+    #creates a new stockpile if one hasn't been created
+    if stockpiles.__len__() == 0:
+        s = AppleStockpiles(random.randint(50, 750), random.randint(50, 550), appleW, appleH)
+        stockpiles.append(s)
     #Draws Purplish background. Unneeded due to spongebob background
     screen.fill((150,0,150))
 
     #draws spongebob background
     screen.blit(background, (75,100))
+
     #event listener
     for event in pygame.event.get():
         #stops the game when the x button is pressed
@@ -111,14 +164,33 @@ while running:
         #handles key presses
         if event.type == pygame.KEYDOWN:
             #sets the x and y changes based on what is pressed
-            if event.key == pygame.K_LEFT:
+            #if both opposite directions are held, player stops
+            if event.key == pygame.K_LEFT and righthold == False:
                 playerX_change = -playerSpeed
-            if event.key == pygame.K_RIGHT:
+                lefthold = True
+            elif event.key == pygame.K_LEFT and righthold == True:
+                playerX_change = 0
+                lefthold = True
+            if event.key == pygame.K_RIGHT and lefthold == False:
                 playerX_change = playerSpeed
-            if event.key == pygame.K_UP:
+                righthold = True
+            elif event.key == pygame.K_RIGHT and lefthold == True:
+                playerX_change = 0
+                righthold = True
+
+            if event.key == pygame.K_UP and downhold == False:
                 playerY_change = -playerSpeed
-            if event.key == pygame.K_DOWN:
+                uphold = True
+            elif event.key == pygame.K_UP and downhold == True:
+                playerY_change = 0
+                uphold = True
+            if event.key == pygame.K_DOWN and uphold == False:
                 playerY_change = playerSpeed
+                downhold = True
+            elif event.key == pygame.K_DOWN and uphold == True:
+                playerY_change = 0
+                downhold = True
+            
             #changes the direction of the player's shooting
             if event.key == pygame.K_w:
                 playerDirection = 0
@@ -129,13 +201,37 @@ while running:
             if event.key == pygame.K_a:
                 playerDirection = 3
             if event.key == pygame.K_SPACE:
-                fire_apple(playerX, playerY)
+                if appleBulletCount > 1:
+                    fire_apple(playerX, playerY)
+                    appleBulletCount -= 1
         #handles key lifts
         if event.type == pygame.KEYUP:
-            #stops changes after corresponding keys are lifted
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            #stops changes after corresponding keys are lifted given that no other key is held
+            #if the other direction is held, direction immediately switches to match
+            if event.key == pygame.K_LEFT and righthold == True:
+                playerX_change = playerSpeed
+                lefthold = False
+            elif event.key == pygame.K_RIGHT and lefthold == True:
+                playerX_change = -playerSpeed
+                righthold = False
+            elif event.key == pygame.K_RIGHT:
+                righthold = False
                 playerX_change = 0
-            if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+            elif event.key == pygame.K_LEFT:
+                lefthold = False
+                playerX_change = 0
+
+            if event.key == pygame.K_UP and downhold == True:
+                playerY_change = playerSpeed
+                uphold = False
+            elif event.key == pygame.K_DOWN and uphold == True:
+                playerY_change = -playerSpeed
+                downhold = False
+            elif event.key == pygame.K_DOWN:
+                downhold = False
+                playerY_change = 0
+            elif event.key == pygame.K_UP:
+                uphold = False
                 playerY_change = 0
 
     #changes the player's position
@@ -153,12 +249,26 @@ while running:
         playerY = screenHeight - playerH
     
     #Bullet movement
-    if apple_state is "fire":
-        appleBulletX += apple_changeX
-        appleBulletY += apple_changeY
-        draw_apple(appleBulletX, appleBulletY)
+    for bullet in bullets:
+        bullet.change()
+        pos = bullet.getPos()
+        x = pos[0]
+        y = pos[1]
+        draw_apple(x, y)
+        if x < 0 or x > screenWidth or y < 0 or y > screenHeight:
+            bullets.remove(bullet)
+            del bullet
+
+    #checks for collision for all stockpiles
+    for pile in stockpiles:
+        pos = pile.getPos()
+        draw_apple(pos[0], pos[1])
+        #removes a pile if collided and adds to the apple bullet count
+        if pile.checkCollision(playerX, playerY, playerW, playerH):
+            stockpiles.remove(pile)
+            del pile
+            appleBulletCount += 1
 
     #draws the players and apples
     player(playerX, playerY)
-    apple(appleX, appleY)
     pygame.display.update()
